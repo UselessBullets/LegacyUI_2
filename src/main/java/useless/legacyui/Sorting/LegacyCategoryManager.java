5d1bf81ad790c62dd8a8ffa6420f99258c9dc257
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-09-15 12:49:01 -0500

    Finished adding Categories

--- src/main/java/useless/legacyui/Sorting/LegacyCategoryManager.java
+++ src/main/java/useless/legacyui/Sorting/LegacyCategoryManager.java
@@ -21,6 +21,9 @@ public class LegacyCategoryManager {
         LegacyCategoryManager.recipeCategories.add(recipeTools.category);
         LegacyCategoryManager.recipeCategories.add(recipeFood.category);
         LegacyCategoryManager.recipeCategories.add(recipeRedstone.category);
+        LegacyCategoryManager.recipeCategories.add(recipeTransit.category);
+        LegacyCategoryManager.recipeCategories.add(recipeMisc.category);
+        LegacyCategoryManager.recipeCategories.add(new RecipeCategory(MOD_ID, "modded", IconHelper.getOrCreateIconTexture(MOD_ID, "modded.png"), recipeMisc.category.getRecipeGroups(false)));
     }
     static {
 
@@ -93,15 +96,71 @@ public class LegacyCategoryManager {
                 new RecipeGroup[]{planks, torches, utilityBlocks, chest, bed, fences, fencegates, woodStairs, woodSlabs, ladders, doors, books, display});
     }
     public static class recipeBricks {
-        public static RecipeGroup blocks = new RecipeGroupBuilder()
-                .addClass(Block.class)
-                .addKeyword("tile")
+        public static RecipeGroup stoneStairs = new RecipeGroupBuilder()
+                .addKeyword(".stairs.")
+                .build();
+        public static RecipeGroup stoneSlabs = new RecipeGroupBuilder()
+                .addKeyword(".slab.")
+                .build();
+        public static RecipeGroup bricks = new RecipeGroupBuilder()
+                .addKeyword(".brick.")
+                .build();
+        public static RecipeGroup polished = new RecipeGroupBuilder()
+                .addKeyword(".polished")
+                .addKeyword(".pillar")
+                .addKeyword(".carved")
+                .excludeItem(Block.pumpkin.asItem())
+                .excludeItem(Block.pumpkinCarvedActive.asItem())
+                .excludeItem(Block.pumpkinCarvedIdle.asItem())
+                .build();
+        public static RecipeGroup wool = new RecipeGroupBuilder()
+                .addClass(BlockWool.class)
+                .build();
+        public static RecipeGroup natural = new RecipeGroupBuilder()
+                .addItem(Block.sandstone)
+                .addItem(Block.gravel)
+                .addItem(Block.blockSnow)
+                .addItem(Block.blockClay)
+                .addKeyword(".cobble.")
+                .excludeItem(Block.dispenserCobbleStone.asItem())
+                .addClass(BlockIce.class)
+                .addItem(Block.permaice)
+                .addClass(BlockPumpkin.class)
+                .addClass(BlockSponge.class)
+                .build();
+        public static RecipeGroup layers = new RecipeGroupBuilder()
+                .addClass(BlockLayerBase.class)
+                .build();
+        public static RecipeGroup resourceBlocks = new RecipeGroupBuilder()
+                .addItem(Block.blockGold)
+                .addItem(Block.blockIron)
+                .addItem(Block.blockDiamond)
+                .addItem(Block.blockLapis)
+                .addItem(Block.blockSteel)
+                .addItem(Block.blockRedstone)
+                .addItem(Block.blockQuartz)
+                .addItem(Block.blockCoal)
+                .addItem(Block.blockCharcoal)
+                .addItem(Block.blockNetherCoal)
+                .addItem(Block.blockOlivine)
+                .build();
+        public static RecipeGroup resourceBlocksUncompacts = new RecipeGroupBuilder()
+                .addItem(Item.ingotGold)
+                .addItem(Item.ingotIron)
+                .addItem(Item.diamond)
+                .addItem(Item.dye, 4, true)
+                .addItem(Item.ingotSteel)
+                .addItem(Item.dustRedstone)
+                .addItem(Item.quartz)
+                .addItem(Item.coal)
+                .addItem(Item.nethercoal)
+                .addItem(Item.olivine)
                 .build();
         public static RecipeCategory category = new RecipeCategory(
                 MOD_ID,
                 "bricks",
                 IconHelper.getOrCreateIconTexture(MOD_ID, "bricks.png"),
-                new RecipeGroup[]{blocks});
+                new RecipeGroup[]{bricks, polished, stoneStairs, stoneSlabs, wool, natural, layers, resourceBlocks, resourceBlocksUncompacts});
     }
     public static class recipeTools {
         public static RecipeGroup pickaxe = new RecipeGroupBuilder()
@@ -151,6 +210,8 @@ public class LegacyCategoryManager {
                 .build();
         public static RecipeGroup chestplate = new RecipeGroupBuilder()
                 .addKeyword(".chestplate.")
+                .addClass(ItemQuiver.class)
+                .addClass(ItemQuiverEndless.class)
                 .build();
         public static RecipeGroup leggings = new RecipeGroupBuilder()
                 .addKeyword(".leggings.")
@@ -188,22 +249,106 @@ public class LegacyCategoryManager {
         public static RecipeGroup sugar = new RecipeGroupBuilder()
                 .addItem(Item.dustSugar)
                 .build();
+        public static RecipeGroup foodGeneral = new RecipeGroupBuilder()
+                .addClass(ItemFood.class)
+                .build();
         public static RecipeCategory category = new RecipeCategory(
                 MOD_ID,
                 "food",
                 IconHelper.getOrCreateIconTexture(MOD_ID, "health.png"),
-                new RecipeGroup[]{bread,stew,cake,cookies,icecream,apple,sugar});
+                new RecipeGroup[]{bread,stew,cake,cookies,icecream,apple,sugar, foodGeneral});
     }
     public static class recipeRedstone {
         public static RecipeGroup basicRedstone = new RecipeGroupBuilder()
                 .addItem(Item.repeater)
-                .addItem(Item.dustRedstone)
-                .addItem(Block.torchRedstoneIdle)
+                .addItem(Item.dustRedstone, true)
+                .addItem(Block.torchRedstoneActive)
                 .build();
+        public static RecipeGroup redstoneBlock = new RecipeGroupBuilder()
+                .addClass(BlockRedstone.class, true)
+                .build();
+        public static RecipeGroup buttonLever = new RecipeGroupBuilder()
+                .addClass(BlockButton.class)
+                .addClass(BlockLever.class)
+                .build();
+        public static RecipeGroup pressureplates = new RecipeGroupBuilder()
+                .addClass(BlockPressurePlate.class)
+                .build();
+        public static RecipeGroup pistons = new RecipeGroupBuilder()
+                .addClass(BlockPistonBase.class)
+                .build();
+        public static RecipeGroup lamps = new RecipeGroupBuilder()
+                .addClass(BlockLamp.class)
+                .build();
+        public static RecipeGroup noteBlock = new RecipeGroupBuilder()
+                .addClass(BlockNote.class)
+                .build();
+        public static RecipeGroup dispensers = new RecipeGroupBuilder()
+                .addClass(BlockDispenser.class)
+                .build();
+        public static RecipeGroup traps = new RecipeGroupBuilder()
+                .addClass(BlockTNT.class)
+                .addClass(BlockSpikes.class)
+                .addClass(BlockMesh.class)
+                .build();
+
         public static RecipeCategory category = new RecipeCategory(
                 MOD_ID,
                 "redstone",
                 IconHelper.getOrCreateIconTexture(MOD_ID, "lever.png"),
-                new RecipeGroup[]{basicRedstone});
+                new RecipeGroup[]{basicRedstone, redstoneBlock, buttonLever, pressureplates,pistons,lamps,noteBlock,dispensers,traps});
+    }
+    public static class recipeTransit {
+        public static RecipeGroup baseRail = new RecipeGroupBuilder()
+                .addItem(Block.rail)
+                .build();
+        public static RecipeGroup detectRail = new RecipeGroupBuilder()
+                .addItem(Block.railDetector)
+                .build();
+        public static RecipeGroup powerRail = new RecipeGroupBuilder()
+                .addItem(Block.railPowered)
+                .build();
+        public static RecipeGroup allRail = new RecipeGroupBuilder()
+                .addClass(BlockRail.class)
+                .build();
+        public static RecipeGroup baseCart = new RecipeGroupBuilder()
+                .addItem(Item.minecart)
+                .build();
+        public static RecipeGroup chestCart = new RecipeGroupBuilder()
+                .addItem(Item.minecartChest)
+                .build();
+        public static RecipeGroup furnaceCart = new RecipeGroupBuilder()
+                .addItem(Item.minecartFurnace)
+                .build();
+        public static RecipeGroup allCart = new RecipeGroupBuilder()
+                .addClass(ItemMinecart.class)
+                .build();
+        public static RecipeGroup boat = new RecipeGroupBuilder()
+                .addClass(ItemBoat.class)
+                .build();
+        public static RecipeCategory category = new RecipeCategory(
+                MOD_ID,
+                "travel",
+                IconHelper.getOrCreateIconTexture(MOD_ID, "rail.png"),
+                new RecipeGroup[]{baseRail,powerRail,detectRail,allRail,baseCart,chestCart,furnaceCart,allCart,boat});
+    }
+    public static class recipeMisc{
+        public static RecipeGroup dyes = new RecipeGroupBuilder()
+                .addClass(ItemDye.class)
+                .build();
+        public static RecipeGroup allBlocks = new RecipeGroupBuilder()
+                .addClass(Block.class)
+                .build();
+        public static RecipeGroup allItems = new RecipeGroupBuilder()
+                .addClass(Item.class)
+                .build();
+        public static RecipeGroup all = new RecipeGroupBuilder()
+                .addKeyword(".")
+                .build();
+        public static RecipeCategory category = new RecipeCategory(
+                MOD_ID,
+                "misc",
+                IconHelper.getOrCreateIconTexture(MOD_ID, "painting.png"),
+                new RecipeGroup[]{dyes,allBlocks,allItems,all});
     }
 }
